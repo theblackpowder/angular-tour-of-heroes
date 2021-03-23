394c2cee9f9cf146217bbcc0f195f4fa0ac2d601
theblackpowder <dylan.schaefer@ndus.edu>
2021-03-22 22:01:44 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component, VERSION } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = "Tour of Heroes";
 }
-
